59f5fec0036382a962067333e55fd32773de5da9
Halvor Brunt <87039605+Halvor0808@users.noreply.github.com>
2024-04-02 23:08:02 +0200

    TUI using App with Event handling, Focusring, viewports, Editor for searchField. Lacks changing of content

--- app/Main.hs
+++ app/Main.hs
@@ -8,14 +8,15 @@ import Test.Protocol.Parser.TestRequest (testRequest)
 import Tui.Tui (tuiRun)
 
 main :: IO ()
-main = do 
+main = do
+  putStrLn "--- Main has been run! ---\n"
   tests 
-  tuiRun
-  
+  st <- tuiRun
+  print st
+
 
 tests :: IO ()
 tests = do
-  putStrLn "--- Main has been run! ---\n"
   testResponse
   putStrLn "----- Request: -----"
   testRequest
--- app/Tui/Tui.hs
+++ app/Tui/Tui.hs
@@ -28,40 +28,108 @@ import Lens.Micro
 import Protocol.Data.Gemtext (Line(..))
 
 
-data Name = PageContent
+data Name = PageContent | SearchField
   deriving (Ord, Eq, Show)
 
-tuiRun :: IO ()
-tuiRun = do 
-    simpleMain ui
+data St =
+  St { _focusRing :: F.FocusRing Name
+     , _searchField :: E.Editor String Name
+     , _content :: String --[Line]
+     } deriving (Show)
+makeLenses ''St
 
+drawUi :: St -> [Widget Name]
+drawUi st = [ui]
+    where
+      searchFieldW  = B.border $ F.withFocusRing (_focusRing st) (E.renderEditor (str . unlines)) (_searchField st)
+      footer       = str "Not yet implemeted: Esc/Ctrl-q - quit, Ctrl-h - help"
+      content      = viewport PageContent T.Vertical $ strWrap "Some random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowo Some random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowo"
+      contentArea  = B.borderWithLabel (str "Content-separator")
+        $ padTop (Pad 2) $ dynamicLeftRightPad 3 content
+      --
+      ui =
+        joinBorders $
+        withBorderStyle unicode $
+        B.borderWithLabel (str "GeminiBrowser") $
+        dynamicLeftRightPad 3 $
+        vBox [ hCenter $ padTopBottom 1 $ hLimit 30 searchFieldW <=> str "Hit Enter to search"
+            , contentArea
+            , hCenter footer
+            ]
 
-ui :: Widget Name
-ui =
-    joinBorders $
-    withBorderStyle unicode $ 
-    borderWithLabel (str "GeminiBrowser") $ 
-    dynamicLeftRightPad 3 $ 
-    vBox [ searchField
-         , contentArea
-         , footer 
-         ]
+renderLines :: [Line] -> [Widget Name]
+renderLines = map renderLine
 
-searchField, footer :: Widget n
--- replace with actual search bar
-searchField = hCenter $ padTopBottom 1 $ str "search field [__________________]" 
-footer = hCenter $ str "Not yet implemeted: q - quit, h - help"
-
--- use txtWrap instead?
-content, contentShort, contentArea :: Widget Name
-contentArea = borderWithLabel (str "Content-separator")
-      $ padTop (Pad 2) $ vBox [dynamicLeftRightPad 3 content, fill ' ']
-content = viewport PageContent Vertical $ strWrap "Some random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowo Some random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowoSome random content, temporarily. wooooooooooooooooooooooooooow lots of content------------------------------------- mmmmmmmmmmmmmmmmmmmmmmm wowo"
-contentShort = str "Short content."
-
+--temp solution
+-- use txtWrap/strWrap with settings instead?
+renderLine :: Line -> Widget Name
+renderLine (TextLine t)             = str (unpack t)
+renderLine (LinkLine t _)           = str (unpack t)
+renderLine (TogglePreformatMode t)  = str (unpack t)
+renderLine (PreformattedTextLine t) = str (unpack t)
+renderLine (HeadingLine i t)        = str (unpack t)
+renderLine (UnorderedListLine t)    = str (unpack t)
+renderLine (QuoteLine t)            = str (unpack t)
 
 
 {- Placeholder. TODO: make dynamnic ;_;
  -}
 dynamicLeftRightPad :: Int -> Widget n -> Widget n
 dynamicLeftRightPad = padLeftRight
+
+handleEvent :: T.BrickEvent Name e -> EventM Name St ()
+handleEvent (T.VtyEvent (V.EvKey (V.KChar 'q') [V.MCtrl])) = do 
+  M.halt
+handleEvent (T.VtyEvent (V.EvKey (V.KChar 'h') [V.MCtrl])) = return () -- TODO: display help
+handleEvent (T.VtyEvent (V.EvKey V.KEsc [])) 
+  = M.halt
+handleEvent (T.VtyEvent (V.EvKey (V.KChar '\t') [])) 
+  = focusRing %= F.focusNext
+handleEvent (T.VtyEvent (V.EvKey V.KBackTab [])) 
+  = focusRing %= F.focusPrev
+handleEvent ev = do
+  r <- use focusRing
+  case F.focusGetCurrent r of 
+    Just PageContent -> handleEventPageContent ev
+    Just SearchField ->  case ev of
+              (T.VtyEvent (V.EvKey V.KEnter []))
+                -> do 
+                  sf <- use searchField -- note overwriting r
+                  let query = concat $ E.getEditContents sf
+                  let result = query
+                  content .= result
+                  -- TODO: Use network to fetch content. Also: percent-encode quer
+                  return () 
+              _ ->  zoom searchField $  E.handleEditorEvent ev
+    Nothing -> return ()
+
+handleEventPageContent :: T.BrickEvent Name e -> EventM Name St ()
+handleEventPageContent ev@(T.VtyEvent (V.EvKey key [])) = 
+  case key of
+    V.KUp    -> zoom content $ M.vScrollBy (M.viewportScroll PageContent) (-1)
+    V.KDown  -> zoom content $ M.vScrollBy (M.viewportScroll PageContent) 1
+    _        -> return ()
+handleEventPageContent _ = return ()
+
+
+initialState :: St
+initialState =
+  St (F.focusRing [SearchField, PageContent])
+     (E.editor SearchField (Just 1) "")
+     []
+
+app :: M.App St e Name
+app = M.App
+        { M.appDraw = drawUi
+        , M.appChooseCursor = M.showFirstCursor
+        , M.appHandleEvent = handleEvent
+        , M.appStartEvent = return () 
+        , M.appAttrMap = -- Stolen example: change at convenience
+            const $ attrMap
+                defAttr
+                  [ (attrName "selected", black `on` white) ]
+        }
+tuiRun :: IO St
+tuiRun = do
+  M.defaultMain app initialState
+
