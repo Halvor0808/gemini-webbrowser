79d747e73b6d1d4390f526f555fd9b9a8617fb4b
Halvor Brunt <87039605+Halvor0808@users.noreply.github.com>
2024-04-17 14:59:03 +0200

    rm unused type alias

--- app/Protocol/Data/Request.hs
+++ app/Protocol/Data/Request.hs
@@ -1,11 +1,9 @@
 module Protocol.Data.Request (
     Url(..),
-    Request,
 ) where
 
 import Data.ByteString.Char8
 
-type Request = Url
 data Url = Url { scheme :: ByteString
                , authority :: ByteString
                , port :: Int
