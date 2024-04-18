4959e488f24992ff5f0fd72e9169945da88a5b39
Halvor Brunt <87039605+Halvor0808@users.noreply.github.com>
2024-04-18 11:37:19 +0200

    update Request parser

--- app/Protocol/Parser/Request.hs
+++ app/Protocol/Parser/Request.hs
@@ -11,15 +11,15 @@ module Protocol.Parser.Request (
 import Protocol.Data.Request hiding (fragment, query, path, port, authority, scheme)
 import Utils.ParseUtil (isEOL)
 import Data.Attoparsec.ByteString.Char8
-    ( Parser, char, takeWhile1, decimal, isAlpha_ascii, 
+    ( Parser, char, takeWhile1, decimal, isAlpha_ascii,
     isDigit, string, option, takeTill, endOfLine)
 import qualified Data.Attoparsec.ByteString.Char8 as Atto
 import Data.ByteString (ByteString)
-import Control.Applicative ((<|>))
+import Control.Applicative ((<|>), many)
 
 
 pGeminiUrl :: Parser Url
-pGeminiUrl = pUrlGeneral "gemini://"
+pGeminiUrl = pUrlGeneral (option "gemini" "gemini" <* "://")
 
 pUrl :: Parser Url
 pUrl = pUrlGeneral pScheme
@@ -30,33 +30,34 @@ pUrlGeneral :: Parser ByteString -> Parser Url
 pUrlGeneral schemeParser = do
   scheme <- schemeParser
   authority <- pAuthority
-  port <- option 1965 (char ':' >> decimal) --default port = 1965
+  port <- option 1965 (char ':' >> decimal)
   path <- pPath
   query <- option "" (char '?' >> takeTill isEOL)
   fragment <- option "" (char '#' >> takeTill isEOL)
   return (Url scheme authority port path query fragment)
 
 pScheme :: Parser ByteString
-pScheme = takeWhile1 (`elem` legalChars)
+pScheme = takeWhile1 (`elem` legalChars) <* "://"
     where
       legalChars = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "+-."
 
 pAuthority :: Parser ByteString
 pAuthority = option "" $ do
-  _ <- "://"
   Atto.takeWhile (\c -> c/= '/' && c /= '?' && c /= '#' && c/= '\r' && c/= '\n')
 
 pPath :: Parser ByteString
-pPath = do 
-   pPath' <|> "/"
+pPath = do
+   option "/" pPath'
 
 pPath' :: Parser ByteString
 pPath' = do
-  path <- mconcat <$> Atto.many1 pSubPath <* option "" "/"
-  return $ path <> "/"
-  where 
+  path <- mconcat <$> many pSubPath
+  trail <- option "/" "/"
+  return $ path <> trail
+  where
     isLegalPathChar c = elem c $ ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "-_.~!$&()+*"
     pSubPath = do
-      rest <- "/" *> Atto.takeWhile1 isLegalPathChar
-      return $ "/" <> rest
-      
+      "/"
+      rest <- Atto.takeWhile1 isLegalPathChar
+      return ("/"<> rest)
+
