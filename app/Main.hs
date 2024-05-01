4ae1cd25be9aa8c9f2768d5034cced528cd984c3
Halvor Brunt <87039605+Halvor0808@users.noreply.github.com>
2024-05-01 16:50:03 +0200

    testing of socket

--- app/Main.hs
+++ app/Main.hs
@@ -6,12 +6,15 @@ module Main where
 import Test.Protocol.Parser.TestResponse (testResponse)
 import Test.Protocol.Parser.TestRequest (testRequest)
 import Tui (tuiRun)
+import Socket (sockTests)
 
 main :: IO ()
 main = do
   putStrLn "--- Main has been run! ---\n"
   st <- tuiRun
   print st
+  -- sockTests
+
 
 
 tests :: IO ()
--- app/Socket.hs
+++ app/Socket.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
-module Socket ( retrievePage ) where
+module Socket ( retrievePage, sockTests, getPage ) where
 
 import qualified Control.Exception as E
 import qualified Data.ByteString.Char8 as C
@@ -29,3 +29,38 @@ retrievePage url = do
       recv ctx >>= \case
         Nothing -> return mempty
         Just chunk -> (chunk <>) <$> recvAll ctx
+
+
+-- TESTING
+getPage :: C.ByteString -> IO C.ByteString
+getPage url = do
+  let info         = map C.unpack $ C.split '/' url
+      [host, path] = case info of
+                      [host, path] -> [host, path]
+                      [host] -> [host, "/"]
+  params <- addCallback <$> newDefaultClientParams (host, ":1965")
+  connect params host "1965" $ \(ctx,_) -> do
+    send ctx ("gemini://" <> C.pack host <> C.pack path <> "\r\n")
+    recvAll ctx
+  where
+    recvAll ctx = do
+      recv ctx >>= \case
+        Nothing -> return mempty
+        Just chunk -> (chunk <>) <$> recvAll ctx
+
+
+
+t0 = Url {scheme = "gemini", authority = "geminiprotocol.net", port = 1965, path = "/", query = "", fragment = ""}
+t1 = Url {scheme = "gemini", authority = "kennedy.gemi.dev", port = 1965, path = "/kennedy.gmi", query = "", fragment = ""}
+t2 = Url {scheme = "gemini", authority = "kennedy.gemi.dev", port = 1965, path = "/docs/search.gmi", query = "", fragment = ""}
+
+
+sockTests = do
+  -- print "Retrieving geminiprotocol.net"
+  -- retrievePage t0 >>= print
+  -- putStrLn "\n"
+  print "Retrieving kennedy.gmi"
+  retrievePage t1 >>= print
+  putStrLn "\n"
+  print "Retrieving search.gmi"
+  retrievePage t2 >>= print
