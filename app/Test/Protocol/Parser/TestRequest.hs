fe0cf1378b18fdf2f2b03f997102d1d88745042d
Halvor Brunt <87039605+Halvor0808@users.noreply.github.com>
2024-05-07 16:48:04 +0200

    update readability TestRequest

--- app/Test/Protocol/Parser/TestRequest.hs
+++ app/Test/Protocol/Parser/TestRequest.hs
@@ -17,32 +17,33 @@ testRequest = do
   testUrl
   testPath
 
--- They do not fail unless unaccepted 
 testUrl :: IO ()
 testUrl = do
   putStrLn "----- Url: -----"
-  testParserIO pUrl "https://geminiprotocol.net/" True 
-    (Done "" (Url "https" "geminiprotocol.net" 1965 "/" "" "")) 
--- recovers trailing "/"
-  testParserIO pUrl "gemini://geminiprotocol.net" True 
-    (Done "" (Url "gemini" "geminiprotocol.net" 1965 "/" "" ""))
--- recovers trailing "/"
-  testParserIO pUrl "gemini://geminiprotocol.net/path/to/file" True           
-    (Done "" (Url "gemini" "geminiprotocol.net" 1965 "/path/to/file/" "" ""))
--- Done w/ remainder
-  testParserIO pUrl "gemini://geminiprotocol.net/errPath//" False             
-    (Done "/" (Url "gemini" "geminiprotocol.net" 1965 "/errPath/" "" ""))
--- Done w/ remainder
-  testParserIO pUrl "gemini://test.net/path/err//to/fail" False               
-    (Done "/to/fail" (Url "gemini" "test.net" 1965 "/path/err/" "" ""))
+  simple
+  recoverTrailSlash1
+  recoverTrailSlash2
+  simpleRemainder
+  simpleRemainder2
+  where
+    simple             = testParserIO pUrl "https://geminiprotocol.net/" True 
+                            (Done "" (Url  "https" "geminiprotocol.net" 1965 "/" "" "")) 
+    recoverTrailSlash1 = testParserIO pUrl "gemini://geminiprotocol.net" True 
+                            (Done "" (Url  "gemini" "geminiprotocol.net" 1965 "/" "" ""))
+    recoverTrailSlash2 = testParserIO pUrl "gemini://geminiprotocol.net/path/to/file" True           
+                            (Done "" (Url  "gemini" "geminiprotocol.net" 1965 "/path/to/file/" "" ""))
+    simpleRemainder    = testParserIO pUrl "gemini://geminiprotocol.net/errPath//" False             
+                            (Done "/" (Url "gemini" "geminiprotocol.net" 1965 "/errPath/" "" ""))
+    simpleRemainder2   = testParserIO pUrl "gemini://test.net/path/err//to/fail" False               
+                            (Done "/to/fail" (Url "gemini" "test.net" 1965 "/path/err/" "" ""))
 
 testPath :: IO ()
 testPath = do
   putStrLn "----- Path: -----"
-  testParserIO pPath "/path/to/file" True  (Done "" "/path/to/file/") -- /path/to/file/
+  testParserIO pPath "/path/to/file" True  (Done "" "/path/to/file/")
   testParserIO pPath "/path/"        True  (Done ""  "/path/")
   testParserIO pPath "/"             True  (Done "" "/")
   testParserIO pPath "//"            False (Done "/" "/")
-  testParserIO pPath ""              True  (Done "" "")               -- recovers '/'  -- double check this one!!!
-  testParserIO pPath "path"          False (Done "path" "")           -- no leading '/'
-  testParserIO pPath "path/"         False (Done "path/" "")          -- no leading '/'
+  testParserIO pPath ""              True  (Done "" "")
+  testParserIO pPath "path"          False (Done "path" "")
+  testParserIO pPath "path/"         False (Done "path/" "")
