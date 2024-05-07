c7afd726a4e611209dd2e3a835b05665775cada5
Halvor Brunt <87039605+Halvor0808@users.noreply.github.com>
2024-05-07 12:17:48 +0200

    improved testing function

--- app/Test/Protocol/Parser/TestResponse.hs
+++ app/Test/Protocol/Parser/TestResponse.hs
@@ -5,7 +5,7 @@ module Test.Protocol.Parser.TestResponse (
 ) 
 where
 
-import Test.Utils.ParseUtil (badParseTest, testParser)
+import Test.Utils.ParseUtil (badParseTest, testParserIO)
 import Protocol.Parser.Response
 import Test.Protocol.Parser.TestGemtextParser (testGemtextParser)
 import Utils.ParseUtil (pParameters)
@@ -27,23 +27,23 @@ testResponse = do
 testStatusCode :: IO ()
 testStatusCode = do
   putStrLn "----- StatusCode: -----"
-  print $ testParser pStatusCode "01" (Fail "" [] "" )
-  print $ testParser pStatusCode "10" (Done "" (InputCode   1 0))
-  print $ testParser pStatusCode "20" (Done "" (SuccessCode 2 0))
-  print $ testParser pStatusCode "35" (Done "" (RedirCode   3 5))
-  print $ testParser pStatusCode "69" (Done "" (RequireCertificateCode 6 9))
-  print $ testParser pStatusCode "70" (Fail "" [] "")
+  testParserIO pStatusCode "01" (Fail "" [] "" )
+  testParserIO pStatusCode "10" (Done "" (InputCode   1 0))
+  testParserIO pStatusCode "20" (Done "" (SuccessCode 2 0))
+  testParserIO pStatusCode "35" (Done "" (RedirCode   3 5))
+  testParserIO pStatusCode "69" (Done "" (RequireCertificateCode 6 9))
+  testParserIO pStatusCode "70" (Fail "" [] "")
 
 testParameters :: IO ()
 testParameters = do
   putStrLn "----- Response Parameters: -----"
-  print $ testParser pResponseParams ";format=markdown\r\n"       
+  testParserIO pResponseParams ";format=markdown\r\n"       
                      (Done "\r\n" (Parameters [("format","markdown")]))
-  print $ testParser pResponseParams "; notRight=meta.typing\r\n" 
+  testParserIO pResponseParams "; notRight=meta.typing\r\n" 
                      (Fail " notRight=meta.typing\r\n" [] "")
-  print $ testParser pResponseParams ";typer=sub.typemega;type2=subtype2;typ3=subtyp3;mistake=sub\r\n"
+  testParserIO pResponseParams ";typer=sub.typemega;type2=subtype2;typ3=subtyp3;mistake=sub\r\n"
                      (Done "\r\n" (Parameters [("typer","sub.typemega"),("type2","subtype2"),("typ3","subtyp3"),("mistake","sub")]))
-  print $ testParser pResponseParams ";đu←↓→œ=wrong;\r\n"
+  testParserIO pResponseParams ";đu←↓→œ=wrong;\r\n"
                      (Fail "đu←↓→œ=wrong;\r\n" [] "")
   where 
     pResponseParams = pParameters ';' '='
@@ -51,21 +51,21 @@ testParameters = do
 testMime :: IO ()
 testMime = do
   putStrLn "----- Mime: -----"
-  print $ testParser pMime "text/gemini"
+  testParserIO pMime "text/gemini"
       (Done "" (makeMime (Just ("text", "gemini")) Nothing))
-  print $ testParser pMime "audio/mpeg;hello=world"
+  testParserIO pMime "audio/mpeg;hello=world"
       (Done "" (makeMime (Just ("audio", "mpeg")) (Just (Parameters [("hello","world")]))))
-  print $ testParser pMime "tex t/gemini;candy=nice"  -- spacing error -> default
+  testParserIO pMime "tex t/gemini;candy=nice"  -- spacing error -> default
       (mimeDefaultParams "tex t/gemini;candy=nice")
-  print $ testParser pMime "text/gemini 2;candy=nice" -- spacing error -> text/gem (no params)
+  testParserIO pMime "text/gemini 2;candy=nice" -- spacing error -> text/gem (no params)
       (mimeDefault " 2;candy=nice")
-  print $ testParser pMime "fail/ª™§º©‘’&ŁŒıÐª" -- illegal char -> defaults
+  testParserIO pMime "fail/ª™§º©‘’&ŁŒıÐª" -- illegal char -> defaults
       (mimeDefaultParams "fail/ª™§º©‘’&ŁŒıÐª")
-  print $ testParser pMime "text/gemini;format=gemtext"
+  testParserIO pMime "text/gemini;format=gemtext"
       (Done "" (makeMime (Just ("text", "gemini")) (Just (Parameters [("format","gemtext")]))))
-  print $ testParser pMime "text/word;format=gemtext;name=myFile"
+  testParserIO pMime "text/word;format=gemtext;name=myFile"
       (Done "" (makeMime (Just ("text", "word")) (Just (Parameters [("format","gemtext"),("name","myFile")]))) )
-  print $ testParser pMime "text/gemini;no tRight=meta.typing" -- spacing error -> text/gemini (no params)
+  testParserIO pMime "text/gemini;no tRight=meta.typing" -- spacing error -> text/gemini (no params)
       (mimeDefault ";no tRight=meta.typing")
   where mimeDefault        remainder = Done remainder (makeMime (Just ("text", "gemini")) Nothing)
         mimeDefaultParams remainder = Done remainder (makeMime (Just ("text", "gemini")) (Just (Parameters [("charset","utf-8")])))
@@ -73,26 +73,26 @@ testMime = do
 testResponseParser :: IO ()
 testResponseParser = do
   putStrLn "----- Response: -----"
-  print $ testParser  pResponse "15 Input prompt. Gimme some\r\n"
+  testParserIO  pResponse "15 Input prompt. Gimme some\r\n"
               (Done "" (INPUT (InputCode 1 5) "Input prompt. Gimme some"))
-  print $ testParser  pResponse "1 Input prompt. Gimme some\r\n" -- missing 1 digit
+  testParserIO  pResponse "1 Input prompt. Gimme some\r\n" -- missing 1 digit
               (Fail " Input prompt. Gimme some\r\n" [] "")
-  print $ testParser pResponse "30 gemini://new.url.visit.to/\r\n"
+  testParserIO pResponse "30 gemini://new.url.visit.to/\r\n"
               (Done "" (REDIRECT (RedirCode 3 0) (Url "gemini" "new.url.visit.to" 1965 "/" "" "")) )
-  print $ testParser  pResponse "30 gemini://new.url.visit.to/" -- missing EOL
+  testParserIO  pResponse "30 gemini://new.url.visit.to/" -- missing EOL
               (Fail "" [] "not enough input")
-  print $ testParser pResponse "30 gemini://recover.missing.forward.slash\r\n" -- Recovers missing '/' in path
+  testParserIO pResponse "30 gemini://recover.missing.forward.slash\r\n" -- Recovers missing '/' in path
               (Done "" (REDIRECT (RedirCode 3 0) (Url "gemini" "recover.missing.forward.slash" 1965 "/" "" "")) )
-  print $ testParser  pResponse "40 Error message for 40\r\n"
+  testParserIO  pResponse "40 Error message for 40\r\n"
               (Done "" (ANY_FAIL (TempFailCode 4 0) "Error message for 40"))
-  print $ testParser  pResponse "50 Error message for 50\r\n"
+  testParserIO  pResponse "50 Error message for 50\r\n"
               (Done "" (ANY_FAIL (PermanentFailCode 5 0) "Error message for 50"))
-  print $ testParser  pResponse "60 You need a ceritificate my man\r\n"
+  testParserIO  pResponse "60 You need a ceritificate my man\r\n"
               (Done "" (ANY_FAIL (RequireCertificateCode 6 0) "You need a ceritificate my man"))
   content01 <- C8.readFile "app/Test/Input/response01-success.eg"
-  print $ testParser pResponse content01 expectedResponse01
+  testParserIO pResponse content01 expectedResponse01
   content02 <- C8.readFile "app/Test/Input/response02-success.eg"
-  print $ testParser pResponse content02 expectedResponse02
+  testParserIO pResponse content02 expectedResponse02
   where expectedResponse01 = 
           Done "" (SUCCESS {_statusCode = SuccessCode 2 0
                   , _mime = makeMime (Just ("text", "gemini")) Nothing
--- app/Test/Utils/ParseUtil.hs
+++ app/Test/Utils/ParseUtil.hs
@@ -1,23 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Test.Utils.ParseUtil (
     badParseTest,
-    testParser,
+    testParserIO,
 ) where
 
-import Data.ByteString
+import Data.ByteString.Char8 hiding (putStrLn)
 import Data.Attoparsec.ByteString.Char8
 
 
 badParseTest :: (Show a) => Parser a -> ByteString -> IO ()
 badParseTest p input = print $ feed (parse p input) empty
 
-testParser :: Eq r => Parser r -> ByteString -> IResult ByteString r -> Maybe Bool
-testParser p input = compareParseResult (testParser' p input)
+testParserIO :: (Show r, Eq r) => Parser r -> ByteString -> IResult ByteString r -> IO ()
+testParserIO p input expected = case testParser p input expected of
+    Right True  -> putStrLn "Test passed!"
+    Left (msg, exp, got) -> do
+        putStrLn msg
+        putStrLn $ "\n" <> exp <> "\n"
+        putStrLn got
 
-testParser' :: Parser a -> ByteString -> IResult ByteString a
-testParser' p input = feed (parse p input ) empty
+testParser :: (Show r, Eq r) => Parser r -> ByteString -> IResult ByteString r -> Either (String, String, String) Bool
+testParser p input expected = do
+    let result =  runParser p input
+    case compareParseResult result expected of
+        Just True -> Right True
+        _         -> Left (unpack $ "TEST FAILED ON INPUT: " <> input 
+                          , "EXPECTED: " <> show expected
+                          , "GOT: " <> show result)
+
+runParser :: Parser a -> ByteString -> IResult ByteString a
+runParser p input = feed (parse p input ) empty
 
 compareParseResult :: Eq a => IResult ByteString a -> IResult ByteString a -> Maybe Bool
-compareParseResult (Fail i0 _ _) (Fail i1 _ _) = if i0 == i1 then Just True else Nothing
+compareParseResult (Fail i0 _ _) (Fail i1 _ _) = if i0 == i1 then Just True else Just False
+compareParseResult (Done i0 r0)  (Done i1 r1)  = if i0 == i1 && r0 == r1 then Just True else Just False
 compareParseResult (Partial _)   (Partial _)   = Nothing
-compareParseResult (Done i0 r0)  (Done i1 r1)  = if i0 == i1 && r0 == r1 then Just True else Nothing
 compareParseResult _ _                         = Just False
