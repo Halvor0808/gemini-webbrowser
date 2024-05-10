c54052beb428046e87145de19c64de605b0ee0e3
Halvor Brunt <87039605+Halvor0808@users.noreply.github.com>
2024-05-10 18:36:00 +0200

    update Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
-
-import Test.Protocol.Parser.TestResponse (testResponse)
-import Test.Protocol.Parser.TestRequest (testRequest)
 import Tui (tuiRun)
 import Socket (sockTests)
 
 main :: IO ()
 main = do
-  putStrLn "--- Main has been run! ---\n"
-  st <- tuiRun
-  print st
-  -- sockTests
-
-
-
-tests :: IO ()
-tests = do
-  putStrLn " --- /// Running tests /// --- \n"
-  testResponse
-  testRequest
-
+  tuiRun >>= print
 
